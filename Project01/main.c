6bdd9a3187d086fd162164a87115e7657bc1988a
alien-house <info@alien-house.com>
2017-02-06 00:04:16 -0800

    dd

--- Project01/main.c
+++ Project01/main.c
@@ -27,7 +27,7 @@ struct student{
     short grade;
     char* address;
     short admission_year;
-    char* courses;
+    char** courses;
 };
 struct studentcourse{
     int studentID;
@@ -43,6 +43,8 @@ void makeStudentStructure( char file_name[] );
 void makeStuCourseStructure( char file_name[] );
 int getArrayLength(char** dataArray);
 
+void mainMenu();
+void delay();
 #define MAXUSER 1000
 #define MAXTEXT 50
 #define N 256
@@ -76,24 +78,24 @@ int main(int argc, const char * argv[]) {
     //    printf("%s \n", Course[0].studentID );
     //    printf("%s \n", *(strctArray[1])[0].studentID );
     
-    makeAccountStructure(file_account);
-    makeCourseStructure(file_course);
+//    makeAccountStructure(file_account);
+//    makeCourseStructure(file_course);
     makeStudentStructure(file_student);
-    makeStuCourseStructure(file_studentscourses);
+//    makeStuCourseStructure(file_studentscourses);
     
     Account = (struct account*) realloc(Account, sizeof(struct account ) * accountMaxNum);
     Course  = (struct course*) realloc(Course, sizeof(struct course ) * courseMaxNum);
     Student = (struct student*) realloc(Student, sizeof(struct student ) * studentMaxNum);
     StudentCourse = (struct studentcourse*) realloc(StudentCourse, sizeof(struct studentcourse ) * stuCourseMaxNum);
     
-    printf("%s \n",Student[0].courses);
+//    printf("%s \n",Student[0].courses);
     
 //    for (int i = 0; i < 20; i++) {
 //        printf("%d \n",Account[i].user);
 //        printf("%s\n",Account[i].pass);
 //    }
 //    printf("%d \n",Student[0].grade);
-        showLoginView();
+//        showLoginView();
     //    showMenuOptions();
     
     return 0;
@@ -130,6 +132,8 @@ void enterLogin( void ){
         printf("************************************************************\n");
         printf("Welcome to Cornerstone International College of Canada.\n");
         printf("************************************************************\n");
+        delay(2000);
+        mainMenu();
     }else{
         printf("************************************************************\n");
         printf("Your account does not exist. Please try again!\n");
@@ -191,23 +195,23 @@ void makeStudentStructure( char file_name[] ){
     
     dataArray = loadFile(file_name);
     char** arrayStudentID = getData(dataArray, "studentID");
-    dataLen = getArrayLength(arrayStudentID);
+//    dataLen = getArrayLength(arrayStudentID);
     
-    char** arrayName = getData(dataArray, "name");
-    char** arrayGender = getData(dataArray, "gender");
-    char** arrayGrade = getData(dataArray, "grade");
-    char** arrayAddress = getData(dataArray, "address");
-    char** arrayAdmis = getData(dataArray, "admission_year");
+//    char** arrayName = getData(dataArray, "name");
+//    char** arrayGender = getData(dataArray, "gender");
+//    char** arrayGrade = getData(dataArray, "grade");
+//    char** arrayAddress = getData(dataArray, "address");
+//    char** arrayAdmis = getData(dataArray, "admission_year");
     char** arrayCourses = getData(dataArray, "courses");
     studentMaxNum = dataLen;
     
     for (int i = 0; i < dataLen; i++) {
-        Student[i].studentID = arrayStudentID[i];
-        Student[i].name = arrayName[i];
-        Student[i].gender = arrayGender[i];
-        Student[i].grade = (short)atoi(arrayGrade[i]);
-        Student[i].address = arrayAddress[i];
-        Student[i].admission_year = (short)atoi(arrayAdmis[i]);
+//        Student[i].studentID = arrayStudentID[i];
+//        Student[i].name = arrayName[i];
+//        Student[i].gender = arrayGender[i];
+//        Student[i].grade = (short)atoi(arrayGrade[i]);
+//        Student[i].address = arrayAddress[i];
+//        Student[i].admission_year = (short)atoi(arrayAdmis[i]);
         Student[i].courses = arrayCourses[i];
     }
     
@@ -248,7 +252,71 @@ int getArrayLength(char** dataArray){
 
 
 
+void mainMenu(){
+    int opt;
+    printf("******************************************\n");
+    printf("Select from the options:\n");
+    printf("******************************************\n");
+    printf("¬[1] Print my enrollment certificate\n");
+    printf("¬[2] Print my courses\n");
+    printf("¬[3] Print my transcript\n");
+    printf("¬[4] Print my GPA\n");
+    printf("¬[5] Print my ranking among all students in the college\n");
+    printf("¬[6] List of all available courses\n");
+    printf("¬[7] List of all students\n");
+    printf("¬[8] Logout\n");
+    printf("¬[9] Exit\n");
+    printf("******************************************\n");
+    printf("Enter the number corresponding to each item to proceed:\n");
+    scanf("%d",&opt);
+    switch(opt){
+        case 1:
+            printf("Dear Sir/Madam,\n\n");
+            printf("This is to certify that Mr. [Peter Brown] with student id [7813007] is a student at grade 1 at CICCC. He\n");
+            printf("was admited to our college in 2011 and has taken [1] course(s). Currently he resides at [850 West Vancouver, Vancouver]\n\n\n");
+            printf("If you have any questions, please do not hesitate to contact us.\n");
+            break;
+        case 2:
+            printf("Hi Mr. [Peter Brown],\n");
+            printf("You have taken the following courses:\n");
+            
+            break;
+        case 3:
+            printf("Hi Mr. [Peter Brown],\n");
+            printf("Here is your transcript:\n\n");
+            break;
+        case 4:
+            printf("Hi Mr. [Peter Brown],\n");
+            printf("Your GPA is:\n\n");
+            break;
+        case 5:
+            printf("Hi Mr. [Peter Brown],\n");
+            printf("Your GPA is:[] and therefore your rank is []\n\n");
+            break;
+        case 6:
+            
+            break;
+        case 7:
+            
+            break;
+        case 8:
+            
+            break;
+        case 9:
+            exit(1);
+            break;
+        default:
+            printf("Wrong Option, please try again!\n");
+    }
+    
+}
 
 
-
+void delay(int x){
+   	int c = 1, d = 1;
+    
+    for ( c = 1 ; c <= x ; c++ )
+        for ( d = 1 ; d <= 200000 ; d++ )
+        {}
+}
 
